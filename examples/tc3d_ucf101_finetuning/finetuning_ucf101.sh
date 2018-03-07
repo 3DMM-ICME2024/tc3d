e1d32dbc13238aa48476174a7f1f3da33677eb4e
tc3d <31884964+tc3d@users.noreply.github.com>
2018-03-07 20:03:16 +0800

    Update finetuning_ucf101.sh

--- examples/tc3d_ucf101_finetuning/finetuning_ucf101.sh
+++ examples/tc3d_ucf101_finetuning/finetuning_ucf101.sh
@@ -1,2 +1,2 @@
-mkdir -p LOG_TSN_UCF101_split1
-GLOG_log_dir="./LOG_TSN_UCF101_split1" ../../.build_release/tools/caffe.bin train --solver=solver_r2.prototxt --weights=./tc3d_kinetics_pretrained.caffemodel --gpu=0,1
+mkdir -p LOG_TC3D_UCF101_split1
+GLOG_log_dir="./LOG_TC3D_UCF101_split1" ../../.build_release/tools/caffe.bin train --solver=solver_r2.prototxt --weights=./tc3d_kinetics_pretrained.caffemodel --gpu=0,1
